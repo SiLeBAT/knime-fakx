3e72535d4e7fa9e71cca860eae746c2ce0cf7770
Miguel <miguel.de-alba-aparicio@bfr.bund.de>
2019-05-06 18:08:36 +0200

    Test to check $TRAVIS_BUILD_DIR

--- bintray.sh
+++ bintray.sh
@@ -2,6 +2,8 @@ API="https://api.bintray.com"
 PROPS=";bt_package=update;bt_version=version;publish=1;override=1"
 FOLDER="$TRAVIS_BUILD_DIR/de.bund.bfr.knime.fakx.update/target/repository"
 
+echo $TRAVIS_BUILD_DIR
+
 SUBJECT=fauthent
 REPO=knime
 
