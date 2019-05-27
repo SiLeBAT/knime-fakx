2130c38c7486baeb170225cafe0127492363fb10
Miguel <miguel.de-alba-aparicio@bfr.bund.de>
2019-05-27 15:37:54 +0200

    Fix writer node dialog.
    The dialog picks a file to save to. Before, the dialog was picking an
    existing path (like in reader). Using a flow variable was not affected
    by this issue.

--- de.bund.bfr.knime.fakx.nodes/src/de/bund/bfr/knime/fakx/nodes/writer/WriterNodeDialog.java
+++ de.bund.bfr.knime.fakx.nodes/src/de/bund/bfr/knime/fakx/nodes/writer/WriterNodeDialog.java
@@ -1,17 +1,21 @@
 package de.bund.bfr.knime.fakx.nodes.writer;
 
+import javax.swing.JFileChooser;
+
 import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
 import org.knime.core.node.defaultnodesettings.DialogComponentFileChooser;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
 
 public class WriterNodeDialog extends DefaultNodeSettingsPane {
 
-    WriterNodeDialog() {
+	WriterNodeDialog() {
 
-        // Create settings model and dialog component
-        SettingsModelString stringModel = new SettingsModelString(WriterNodeModel.CFGKEY_FILE, WriterNodeModel.DEFAULT_FILE);
-        DialogComponentFileChooser fileChooser = new DialogComponentFileChooser(stringModel, "writer-history", ".fakx");
+		// Create settings model and dialog component
+		SettingsModelString stringModel = new SettingsModelString(WriterNodeModel.CFGKEY_FILE,
+				WriterNodeModel.DEFAULT_FILE);
+		DialogComponentFileChooser fileChooser = new DialogComponentFileChooser(stringModel, "writer-history",
+				JFileChooser.SAVE_DIALOG, ".fakx");
 
-        addDialogComponent(fileChooser);
-    }
+		addDialogComponent(fileChooser);
+	}
 }
